3b27c1453ce02659b9ebc045de2e1906039d35d0
Sanjaye3 <92200293+Sanjaye3@users.noreply.github.com>
2023-08-28 16:10:19 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,35 @@
+// var,let,const
+let num1 = 5;
+let num2 = 6;
+let sum = num1 + num2;
+console.log(sum)
+console.log(num1*num2);
+
+// print statement
+
+console.log("")
+// == equal to
+// === equal to and equal type
+
+let name = "sanjaye";
+let name2 = "sanjaye";
+
+if(name == name2){
+    console.log("both names are equal");
+}
+else{
+    console.log("not equal");
+}
+
+
+let number = 100 ;
+let number2 = "100";
+
+if(number === number2){
+    console.log("both names are equal");
+}
+else{
+    console.log("not equal");
+}
+
+ 

b69fa49406e49d8627ecf7dd3b673b8c9b5f0427
Sanjaye3 <92200293+Sanjaye3@users.noreply.github.com>
2023-09-04 16:52:59 +0530

    Add files via upload

--- basic.js
+++ basic.js
@@ -6,7 +6,7 @@ console.log(sum)
 console.log(num1*num2);
 
 // print statement
-
+ 
 console.log("")
 // == equal to
 // === equal to and equal type
@@ -33,3 +33,38 @@ else{
 }
 
  
+
+// primitive data types - the predefined data types that are provided by js. eg- string,number,boolean,undefined,null
+
+
+// non primitive data types- these are the datatypes that are derived from the peimitive data types.
+
+// eg- Array,object 
+
+// array- [1,2,3,4,5] array is a collection of values, when you have requirement to store a group of values.
+
+// object- object is an instance and have key value pair and can be accessed by dot operator.
+
+
+// let object = {
+//     name:"sanjaye",
+//     designation:"sd",
+//     gender:"male"
+// }
+
+
+// object.gender
+
+
+
+// function is a block of code,that is executed once when you call the function.
+
+
+ 
+
+function sum(){
+    
+    console.log("today is 04/09");
+}
+
+sum()
---
+++ jsd2.js
@@ -0,0 +1,60 @@
+// function sum(){
+    
+//     console.log("today is 04/09");
+// }
+
+ 
+
+
+let array = [1,2,3,4,5];
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+
+
+
+
+
+
+console.log(typeof(array));
+
+
+// implicit type conversion
+let num1 = "5";
+let num2 = "9";
+
+let  sum = num1 + num2 ;
+
+console.log(sum);
+// 14
+
+
+let number= 10;
+console.log(typeof(number));
+let numberr="10";
+console.log(typeof(numberr));
+
+
+ 
+
+// let object = {
+//     name:"sanjaye",
+//     designation:"sd",
+//     gender:"male"
+// }
+
+
+// json => javascript object notation
+
+// it gives you data from server,it is represented as json
+
+
+
+let object =
+        {
+        name:"sanjaye",
+        designation:"sd",
+        gender:"male"
+    }
+ 

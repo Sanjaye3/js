7888e29f34dfbbb37a19fa8abdd724e7398eb127
Sanjaye3 <92200293+Sanjaye3@users.noreply.github.com>
2023-09-05 15:56:38 +0530

    Add files via upload

--- jsd2.js
+++ jsd2.js
@@ -8,16 +8,16 @@
 
 let array = [1,2,3,4,5];
 
-console.log(array[0]);
-console.log(array[1]);
-console.log(array[2]);
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
 
 
 
 
 
 
-console.log(typeof(array));
+// console.log(typeof(array));
 
 
 // implicit type conversion
@@ -26,14 +26,14 @@ let num2 = "9";
 
 let  sum = num1 + num2 ;
 
-console.log(sum);
+// console.log(sum);
 // 14
 
 
 let number= 10;
-console.log(typeof(number));
+// console.log(typeof(number));
 let numberr="10";
-console.log(typeof(numberr));
+// console.log(typeof(numberr));
 
 
  
@@ -58,3 +58,109 @@ let object =
         gender:"male"
     }
  
+
+
+
+    let example =["chennai","bangalore","coimbatore"];
+
+
+for(let city of example){
+    console.log(city);
+}
+
+for (let city in example){
+    console.log(city)
+}
+
+
+// for in => iteration over the keys(index)
+
+// for of => data itself.
+
+
+
+
+function name(){
+    console.log("this is a fucntion");
+}
+
+
+name()
+
+
+// arguments is an array-like object accessible inside functions that contains the values of the arguments passed to that function.
+
+function summ(a,b,c){
+    console.log(a+b+c);
+}
+
+summ(25,25,25)
+
+summ(1,3,2)
+
+
+
+function getfood(name){
+    console.log("food is for" + " " + name);
+}
+
+getfood("sanjauye")
+
+getfood("agilan")
+
+
+
+// anonymous functions = > these are the functions which do not have name.
+
+// an anonymous functions is not accesible after its initial creation,it can be only accessed by a variable
+
+
+let anon = function (){
+    console.log("this is a anonymous function");
+}
+
+ anon()
+
+ let example2 = function (name){
+    console.log("food is for" + " " + name);
+}
+
+example2("agilan")
+
+
+// iife functions => immediately invoked function expression
+    // where we called it immediately,calls only 1 time
+
+// (function (){
+//     console.log("this is a IIFE function");
+// }) ();  
+
+
+
+// regular function
+function iife (){
+    console.log("this is a IIFE function");
+}
+
+iife();
+
+
+// arrow function (es6)
+
+// arrow functions allows us to write shorter function syntax
+
+
+// optimized version to normal function
+
+
+let summm = (num1,num2) => {
+    console.log(num1+num2);
+} 
+
+summm(1,2)
+
+
+// Sum of all numbers
+// convert te string to uppercase
+
+
